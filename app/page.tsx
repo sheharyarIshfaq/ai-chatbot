1cea7cc8b886528737d32648205797342a1af963
Sheharyar Ishfaq <sheharyarishfaq@gmail.com>
2024-10-19 13:02:58 +0500

    Added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Button>Click Me</Button>
     </div>
   );
 }
